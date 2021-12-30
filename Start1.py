1aa6d580f1cc742b5e10fe9466d4944369f0fea7
Gabriela Cisneros V <93209013+GabyCisnerosV@users.noreply.github.com>
2021-12-30 13:48:49 +0000

    test array text and counting of word

--- Start1.py
+++ Start1.py
@@ -5,3 +5,13 @@ Created on Thu Dec 30 12:38:46 2021
 @author: gaby
 """
 
+text=["London Paris London","Paris Paris London"]
+#Representation of text as vectors
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(text)
+
+#Visualization. Each row is is each string from the text
+print(cv.get_feature_names()) #feature names
+print(count_matrix.toarray()) #count for each one
+
